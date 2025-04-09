f50f902aec43f0318c759b1eec7f9a970615564a
mingkyeongg <ming0820@naver.com>
2025-04-09 14:08:34 +0900

    feat: mynumber.c 파일 수정

--- mynumber.c
+++ mynumber.c
@@ -2,15 +2,46 @@
 
 int main(void)
 {
+  printf("Odd Number : ");
   for (int i = 1; i <= 100; i++)
   {
     if (i % 2 == 1)
     {
-      printf("Odd Number : %d\n", i);
-    }
-    else
-    {
-      printf("Even Number : %d\n", i);
+      printf("%d ", i);
     }
   }
+  printf("\n");
+
+  printf("Even Number : ");
+  for (int i = 1; i <= 100; i++)
+  {
+    if (i % 2 == 0)
+    {
+      printf("%d ", i);
+    }
+  }
+  printf("\n");
+
+  printf("Prime Number : ");
+  for (int i = 2; i <= 100; i++)
+  {
+    int is_prime = 1;
+
+    for (int j = 2; j * j <= i; j++)
+    {
+      if (i % j == 0)
+      {
+        is_prime = 0;
+        break;
+      }
+    }
+
+    if (is_prime)
+    {
+      printf("%d ", i);
+    }
+  }
+
+  return 0;
+  printf("\n");
 }
